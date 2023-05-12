60dd9f0e901d50bb7df4aafbe16b4bd763b2fc11
DevHolako <holakonoob@gmail.com>
2023-05-12 22:10:26 +0100

    add refresh & logout routes

--- src/routes/authroute.ts
+++ src/routes/authroute.ts
@@ -1,11 +1,18 @@
-import { LoginHandler, RegisterHandler } from "@/controllers/AuthController";
+import {
+  LoginHandler,
+  LogoutHandler,
+  RefreshToken,
+  RegisterHandler,
+} from "@/controllers/AuthController";
 import { createUserSchema } from "@/intrefaces/User";
 import validate from "@/middlewares/validateUserReq";
 import { Router } from "express";
 
 const AuthRoutes = Router();
 
-AuthRoutes.post("/login", LoginHandler);
 AuthRoutes.post("/register", validate(createUserSchema), RegisterHandler);
+AuthRoutes.post("/login", LoginHandler);
+AuthRoutes.get("/refresh", RefreshToken);
+AuthRoutes.post("/logout", LogoutHandler);
 
 export { AuthRoutes };
