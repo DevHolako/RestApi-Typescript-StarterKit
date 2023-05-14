0a07c55b7e33db028594cfe6610cbbb88d8bc6ce
DevHolako <holakonoob@gmail.com>
2023-05-14 03:12:50 +0100

    update env

--- environment.d.ts
+++ environment.d.ts
@@ -4,8 +4,6 @@ declare global {
       NODE_ENV: string;
       PORT: number;
       MONGO_URI: string;
-      TOKEN_EXPIERTIME: number;
-      TOKEN_ISSUER: string;
       TOKEN_SECRET: string;
       FRONTEND_URL: string;
     }
